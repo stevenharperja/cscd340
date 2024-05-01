703909e6f25f2645e6d9d72e8a119632304c08a9
Steven Harper <20556807+stevenharperja@users.noreply.github.com>
2024-05-01 22:36:49 +0000

    add safe fix

--- 13-virtual-memory-intro/memory-user.c
+++ 13-virtual-memory-intro/memory-user.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+    if(argc == 1){
+        printf("Usage: %s <num-mb>\n", argv[0]);
+        return 1;
+    }
     int num_mb = atoi(argv[1]);
     int size = num_mb * (1<<20);
     unsigned char *bytes = malloc(size);
