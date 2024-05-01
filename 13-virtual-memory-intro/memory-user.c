59e6f270ca31d0590cd4d5fb5d000a0650ea0511
Steven Harper <20556807+stevenharperja@users.noreply.github.com>
2024-05-01 21:51:38 +0000

    add code from slide

---
+++ 13-virtual-memory-intro/memory-user.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int num_mb = atoi(argv[1]);
+    int size = num_mb * (1<<20);
+    unsigned char *bytes = malloc(size);
+    int i;
+    while(1) {
+        for(i = 0; i < size; i++) {
+            bytes[i] = bytes[i % size] + 1;
+        }
+    }
+    return 0;
+}
